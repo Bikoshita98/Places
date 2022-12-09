5dbe927e226a2ade153e6ce30af79b672c814df8
Bikoshita Gogoi <bikoshitagogoi@gmail.com>
2022-12-09 18:06:05 +0530

    initial commit

--- util/location.js
+++ util/location.js
@@ -6,29 +6,31 @@ const HttpError = require('../models/http-error');
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
-  );
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516
+  };
 
-  const data = response.data;
+  // const response = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+  //     address
+  //   )}&key=${API_KEY}`
+  // );
 
-  if (!data || data.status === 'ZERO_RESULTS') {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
-    );
-    throw error;
-  }
+  // const data = response.data;
+  // console.log("data is", data);
 
-  const coordinates = data.results[0].geometry.location;
+  // if (!data || data.status === 'ZERO_RESULTS') {
+  //   const error = new HttpError(
+  //     'Could not find location for the specified address.',
+  //     422
+  //   );
+  //   throw error;
+  // }
 
-  return coordinates;
+  // const coordinates = data.results[0].geometry.location;
+
+  // return coordinates;
 }
 
 module.exports = getCoordsForAddress;
